e80222af809bfc0883d5169920baa990dd0dd414
gari1311 <garimagautam1311.com>
2021-06-01 19:15:42 +0530

    ecommerce

---
+++ client/build/precache-manifest.cba804e13be3caa3d4e002bb51b16ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9965f79b1ae375bd9868ade18dc6b5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d47f60aad641af46428",
+    "url": "/static/css/main.daa4f962.chunk.css"
+  },
+  {
+    "revision": "26ddc8772ba0ecc16add",
+    "url": "/static/js/2.8d0b4033.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8d0b4033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d47f60aad641af46428",
+    "url": "/static/js/main.1bf79a13.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
